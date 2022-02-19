ddfdfb8112d40c5de559bdb971e2d0842d965f21
html6405 <peter.schelchshorn@mhs-solutions.at>
2022-02-19 13:49:11 +0100

    libsecril-client-sap: move to vendor and fix build.

--- ril/libsecril-client-sap/Android.mk
+++ ril/libsecril-client-sap/Android.mk
@@ -7,6 +7,9 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES:= \
     secril-client-sap.cpp
 
+LOCAL_C_INCLUDES += \
+	hardware/ril/include
+
 LOCAL_SHARED_LIBRARIES := \
     libutils \
     libbinder \
@@ -17,6 +20,7 @@ LOCAL_SHARED_LIBRARIES := \
 LOCAL_CFLAGS := 
 
 LOCAL_MODULE:= libsecril-client-sap
+LOCAL_VENDOR_MODULE := true
 LOCAL_PRELINK_MODULE := false
 
 include $(BUILD_SHARED_LIBRARY)
